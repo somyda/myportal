fd0b3df323c0e7a9bea32a88019d58f2469cb916
somy <somy.park@gmail.com>
2021-04-19 10:22:10 +0900

    게시판 서비스 구현 수정

--- myportal/src/main/java/com/bitacademy/myportal/controller/BoardController.java
+++ myportal/src/main/java/com/bitacademy/myportal/controller/BoardController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class BoardController {
 	private static Logger logger = 
 			LoggerFactory.getLogger(BoardController.class);
 	//	서비스 연결
+	@Autowired
 	BoardService boardServiceImpl;
 	
 	@RequestMapping({"", "/", "/list"})
